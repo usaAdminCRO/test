e45811c330f8bffcc1fb261ea0d422ef3c7fe70c
usaAdminCRO <108192779+usaAdminCRO@users.noreply.github.com>
2022-08-28 00:39:23 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ var express = require("express");
 var app = express();
 
 var metadataRouter = require('./routes/api/token-metadata')
-app.use('/api/token-metadata', metadataRouter);
+app.use('/hidden', metadataRouter);
 
 module.exports = app;
